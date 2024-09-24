1a3e7e6035069dc7250068be3658ff75701da90b
codingkunst <ryoo6931@gmail.com>
2024-09-24 16:07:13 +0900

    [Feat] 컴포넌트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
+import Form from './components/Form';
+import MainCard from './components/MainCard';
 
 function App() {
 
+  const [count, setCount] = useState(1);
+  const countTitle = count === null ? "" : count + "번째";
+  const [mainCat, setMainCat] = useState("https://i.sstatic.net/xcGlN.png");
+
+
   return (
     <>
+      <Header>꽁꽁 얼어붙은 한강 위로<br />{countTitle} 고양이가 걸어다닙니다</Header>
+      <Form />
+      <MainCard />
     </>
   )
 }
